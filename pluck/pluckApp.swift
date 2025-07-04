3ab92c36dc50b7d321638e9ef0888c7a136966d3
asnewman <ashznewman@gmail.com>
2025-07-04 09:39:35 -0700

    Initial Commit

---
+++ pluck/pluckApp.swift
@@ -0,0 +1,17 @@
+//
+//  pluckApp.swift
+//  pluck
+//
+//  Created by Ashley Newman on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct pluckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
